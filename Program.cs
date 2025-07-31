c693ea22b1b1b18a90f8fe5759b274d92f43f640
Jegarde <jesse.tapani.nieminen@gmail.com>
2025-07-31 22:24:46 +0300

    Fallback error if all fails

--- Program.cs
+++ Program.cs
@@ -1,20 +1,27 @@
-﻿using Microsoft.OpenApi.Models;
-using TJ_API.Controllers;
-using TJ_API.Services;
+﻿using TJ_API.Services;
+using TJ_API.Models;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
+// Setup Swagger docs
 SwaggerHandler.BuilderInit(builder);
 
 WebApplication app = builder.Build();
 app.UseRouting();
 
+// Let's not provide the blueprint to our API to hackers! 👻
 if (app.Environment.IsDevelopment())
 {
     SwaggerHandler.Run(app);
 }
 
 app.MapControllers();
+
+app.MapFallback(async ctx =>
+{
+    ctx.Response.StatusCode = 500;
+    await ctx.Response.WriteAsJsonAsync(new ErrorMessage("Muja! Check your arguments. Monnimoka."));
+});
 
 app.Run();
